2fc197753f5d5abf1ca966128a5b85c11a677c8e
Mohamad-Aboda <MohamedAboda@std.mans.edu.eg>
2021-09-21 02:08:10 +0200

    update book style and add postgres database connection

--- OLMS/OLMS/settings.py
+++ OLMS/OLMS/settings.py
@@ -68,11 +68,14 @@ WSGI_APPLICATION = 'OLMS.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'book',
+        'USER': 'postgres', 
+        'PASSWORD':'postgres', 
+        'HOST':'localhost', 
+        'PORT':'5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
